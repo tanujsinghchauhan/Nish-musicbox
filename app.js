5e727ca4c56f3a2e64d87cfe93b1bc6fcb090337
tanujsinghchauhan <tanujsinghchauhan38@gmail.com>
2024-06-10 11:48:09 +0530

    added basic frames too tierd tp add other things

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var preloader = document.getElementById('preloader');
+    // Simulate a delay for demonstration (remove in production)
+    setTimeout(function() {
+        preloader.style.display = 'none';
+    }, 2000); // 2 seconds delay
+});
