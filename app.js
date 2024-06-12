130d2dc86d29b08728aa5f59662155bf8da7fe15
tanujsinghchauhan <tanujsinghchauhan38@gmail.com>
2024-06-12 20:55:50 +0530

    added some sample music and added the player pause and play setting will be updating the rest soon

--- app.js
+++ app.js
@@ -9,3 +9,39 @@ document.getElementById('playPauseSVG').addEventListener('click', function() {
     this.classList.toggle('clicked');
 });
 
+//playing song
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let playPauseSVG = document.getElementById("playPauseSVG");
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
+function playPause() {
+    if (playPauseSVG.classList.contains("pauses")){
+        song.pause();
+        playPauseSVG.classList.remove("pauses");
+        playPauseSVG.classList.add("plays");
+
+    }
+    else {
+        song.play();
+        playPauseSVG.classList.add("pauses");
+        playPauseSVG.classList.remove("plays");
+
+    }
+}
+ if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime ;
+
+    },500);
+ }
+ progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value ;
+    playPauseSVG.classList.add("pauses");
+        playPauseSVG.classList.remove("plays");
+
+ }
